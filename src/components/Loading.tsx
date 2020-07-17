49c4d72b966407a50a845e8158e7e57681ff613a
thothmaat <852529987@qq.com>
2020-07-17 18:43:09 +0800

    delete useless files

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Spin } from 'antd';
 
 const Loading = () => (
-  <div className="cm-loading">
-    <Spin className="cm-loading__spinner" />
-    <span className="cm-loading__text">加载中...</span>
+  <div>
+    <Spin/>
   </div>
 );
 
